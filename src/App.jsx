52303edabcc149ebedb19f84582c550f26d93511
Wessley Silva <wessley.silva@outlook.com.br>
2024-06-22 13:51:26 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+  return (
+    <>
+    <h1 className="text-3xl font-bold underline text-pink">
+      Hello world!
+    </h1>    
+    </>
+  )
+}
+
+export default App
